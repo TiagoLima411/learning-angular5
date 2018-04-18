9244776ff81595fe7c8383e23f2c1df7cf910856
Tiago de lima <tiago_lima411@hotmail.com>
2018-04-17 22:45:26 -0300

    propert bind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div style="text-align:center">
+      <h2>Welcome : {{title}}</h2>
+      <input [id]="myid" placeholder="type input" value="Value of input">
+      <input id={{myid}} placeholder="type input" value="Value of input">
+      <input [disabled]="isDisable" placeholder="type input" value="Value of input">
+      <input bind-disabled="isDisable" placeholder="type input" value="Value of input">
+    </div>
+    `,
+  styles: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+  public myid = 'myIdPropert';
+  public isDisable = true;
 }
