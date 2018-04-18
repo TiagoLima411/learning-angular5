91e7b4b836bc7be543ef0afc2c93110b237a3a49
Tiago de lima <tiago_lima411@hotmail.com>
2018-04-17 23:24:17 -0300

    style binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, style } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,35 +6,25 @@ import { Component } from '@angular/core';
     <div style="text-align:center">
       <h2>Welcome : {{title}}</h2>
       <h2>CodeEvolution</h2>
-      <p class="text-success">CodeEvolution</p>
-      <p [class]="successClass">CodeEvolution</p>
-      <p class="text-special" [class]="successClass">CodeEvolution</p>
-      <p [class.text-danger]="hasError">CodeEvolution</p>
-      <p [ngClass]="messageClasses">CodeEvolution</p>
+
+      <h2 [style.color]="hasError ? 'red' : 'green'">Style Binding</h2>
+      <h2 [style.color]="hightlightColor">Style Binding</h2>
+      <h2 [ngStyle]="titleStyles">Style Binding</h2>
     </div>
     `,
   styles: [`
-    .text-success{
-      color: green;
-    }
-    .text-danger{
-      color: red;
-    }
-    .text-special{
-      font-style: italic;
-    }
+
   `]
 })
 export class AppComponent {
-  title = 'app';
-  public myid = 'myIdPropert';
-  public successClass = "text-success";
-  public hasError = false;
 
+  title = 'app';
+  public hasError = false;
   public isSpecial = true;
-  public messageClasses = {
-    "text-success": !this.hasError,
-    "text-danger": this.hasError,
-    "text-special": this.isSpecial
+  public hightlightColor = "orange";
+  public titleStyles = {
+    color: "blue",
+    fontStyle: "italic"
   }
+
 }
