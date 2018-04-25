df8e1c2d6fc64d703f10f638f35f1dcb4bff216e
Tiago de lima <tiago_lima411@hotmail.com>
2018-04-25 18:42:09 -0300

    Two Way Binding

--- src/app/teste/teste.component.ts
+++ src/app/teste/teste.component.ts
@@ -3,21 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-teste',
   template: `
-    <p>
-      teste works!
-    </p>
-    <h2>Wellcome: {{name}}</h2>
-    <button (click)="clickou()">Greet</button>
-
-    <p>{{greeting}}</p>
-
-    <button (click)="getEvent($event)">Greet</button>
-
-    <p>{{showEvent}}</p>
-
-    <button (click)="anotherTest='variable inserted int html'">Greet</button>
-
-    <p>{{anotherTest}}</p>
+    <input [(ngModel)]="name" type="text">
+    {{name}}
   `,
   styles: [`
 
@@ -25,24 +12,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TesteComponent implements OnInit {
 
-  public name = 'Code Evolution'
-  public greeting = ''
-  public showEvent = ''
-  public anotherTest = ''
+  public name = "";
 
   constructor() { }
 
   ngOnInit() {
   }
-
-  clickou() {
-    this.greeting = "CodeEvolution"
-    console.log("Resposta do evento click");
-  }
-
-  getEvent(event){
-    this.showEvent = event.y;
-    console.log(event);
-  }
-
 }
