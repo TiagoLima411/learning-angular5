2268512dd3fb0f5d66df6d19adba244a4a17b38f
Tiago de lima <tiago_lima411@hotmail.com>
2018-04-26 16:57:10 -0300

    ngSwitchCase

--- src/app/teste/teste.component.ts
+++ src/app/teste/teste.component.ts
@@ -3,16 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-teste',
   template: `
-    <input [(ngModel)]="name" type="text">
-    {{name}}
+    <div [ngSwitch]="color">
+      <div *ngSwitchCase="'red'"> you picked red color</div>
+      <div *ngSwitchCase="'blue'">you picked blue color</div>
+      <div *ngSwitchCase="'black'">you picked black color</div>
+    </div>
   `,
-  styles: [`
-
-  `]
+  styles: [``]
 })
+
 export class TesteComponent implements OnInit {
 
-  public name = "";
+  public color = "red";
 
   constructor() { }
 
