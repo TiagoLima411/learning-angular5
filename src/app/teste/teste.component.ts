f2007ef693deb4cd96511763187165d3b5c11f2c
Tiago de lima <tiago_lima411@hotmail.com>
2018-04-26 17:32:28 -0300

    ngFor

--- src/app/teste/teste.component.ts
+++ src/app/teste/teste.component.ts
@@ -3,16 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-teste',
   template: `
-    <input [(ngModel)]="name" type="text">
-    {{name}}
-  `,
-  styles: [`
+    <div *ngFor="let color of colors; index as i">
+      <h2>{{i}} {{color}}</h2>
+    </div>
 
-  `]
+    <div *ngFor="let color of colors; first as f">
+      <h2>{{f}} {{color}}</h2>
+    </div>
+
+    <div *ngFor="let color of colors; last as l">
+      <h2>{{l}} {{color}}</h2>
+    </div>
+
+    <div *ngFor="let color of colors; even as e">
+      <h2>{{e}} {{color}}</h2>
+    </div>
+  `,
+  styles: []
 })
+
 export class TesteComponent implements OnInit {
 
-  public name = "";
+  public colors = ["red","green","blue","blue"];
 
   constructor() { }
 
