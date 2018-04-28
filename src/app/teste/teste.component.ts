3c69c5d275498ab8056cdf0c0c73e5f517e5d9c1
Tiago de lima <tiago_lima411@hotmail.com>
2018-04-28 16:06:21 -0300

    pipes

--- src/app/teste/teste.component.ts
+++ src/app/teste/teste.component.ts
@@ -4,30 +4,45 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-teste',
   template: `
-    <!-- <h2>{{"Hello " + parentData}}</h2> -->
-    <h2>{{"Hello " + name}}</h2>
+    <h2>{{name}}</h2>
+    <h2>{{name | lowercase}}</h2>
+    <h2>{{name | uppercase}}</h2>
+    <h2>{{message | titlecase}}</h2>
+    <h2>{{name | slice:3:5}}</h2>
+    <h2>{{person | json}}</h2>
 
-    <!-- send event via button -->
-    <button (click)="fireEvent()">Send Event</button>
+    <h2>{{ 5.678 | number:'1.2-3'}}</h2>
+    <h2>{{ 5.678 | number:'3.4-5'}}</h2>
+    <h2>{{ 5.678 | number:'3.1-2'}}</h2>
+
+    <h2>{{ 0.25 | percent}}</h2>
+    <h2>{{ 0.25 | currency}}</h2>
+    <h2>{{ 0.25 | currency: 'GBP'}}</h2>
+    <h2>{{ 0.25 | currency: 'EUR': 'code' }}</h2>
+    <h2>{{ 0.25 | currency: 'R$'}}</h2>
+
+    <h2>{{date}}</h2>
+    <h2>{{date | date: 'short'}}</h2>
+    <h2>{{date | date: 'shortDate'}}</h2>
+    <h2>{{date | date: 'shortTime'}}</h2>
   `,
   styles: []
 })
 
 export class TesteComponent implements OnInit {
-  //two way to use this case from parent to child
-  //@Input() public parentData;
-  @Input('parentData') public name;
 
-  //Sending data from child to parent
-  @Output() public childEvent = new EventEmitter();
+  public name = "Codevolution";
+  public message = "Wellcome to evolutioon";
+  public person = {
+    "firstName": "Jhon",
+    "lastName": "Doe"
+  }
+
+  public date = new Date();
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  //method to fire event
-  fireEvent(){
-    this.childEvent.emit('Hey CodeEvolution');
-  }
 }
